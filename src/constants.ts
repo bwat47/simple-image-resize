8a00e759528e190e58a09bc54bb656a32149eccb
bwat47 <bwat47@gmail.com>
2025-11-21 13:07:33 -0500

    chore: remove more unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -12,19 +12,10 @@ export const REGEX_PATTERNS = {
     IMG_ALT: /\balt\s*=\s*(["'])(.*?)\1/i,
     IMG_TITLE: /\btitle\s*=\s*(["'])(.*?)\1/i,
 
-    // For validating a string that is ONLY a markdown image (resource or external) with optional title and escaped ) support
-    MARKDOWN_IMAGE_ONLY:
-        /^!\[[^\]]*\]\(\s*(?::\/[a-f0-9]{32}|https?:\/\/(?:\\\)|[^)\s])+)?\s*(?:"[^"]*"|'[^']*')?\s*\)$/i,
-
-    // For validating a string that is ONLY an HTML image (resource or external)
-    HTML_IMAGE_ONLY: /^<img\s+[^>]*src=["'](?::\/[a-f0-9]{32}|https?:\/\/[^"']+)["'][^>]*>$/i,
-
     // Global variants for counting occurrences (case-insensitive) - support both formats
     MARKDOWN_IMAGE_GLOBAL:
         /!\[[^\]]*\]\(\s*(?::\/[a-f0-9]{32}|https?:\/\/(?:\\\)|[^)\s])+)?\s*(?:"[^"]*"|'[^']*')?\s*\)/gi,
     HTML_IMAGE_GLOBAL: /<img\s+[^>]*src=["'](?::\/[a-f0-9]{32}|https?:\/\/[^"']+)["'][^>]*>/gi,
-    ANY_IMAGE_GLOBAL:
-        /(?:!\[[^\]]*\]\(\s*(?::\/[a-f0-9]{32}|https?:\/\/(?:\\\)|[^)\s])+)?\s*(?:"[^"]*"|'[^']*')?\s*\))|(?:<img\s+[^>]*src=["'](?::\/[a-f0-9]{32}|https?:\/\/[^"']+)["'][^>]*>)/gi,
 };
 
 export const CONSTANTS = {
